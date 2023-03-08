a9306d43f1f28492db1d7ac7ebe584df7dae92f6
Diego Varzim <diego.varzim@hotmail.com>
2023-03-08 08:41:00 -0300

    ok

--- aula37.py
+++ aula37.py
@@ -1,1 +1,10 @@
-print('Hello world')
+"""
+Repetições 
+While (enquanto) - Looping
+Executa uma ação enquanto uma condição for verdadeira.
+
+Loop infinito -> Quando um código não tem fim.
+
+"""
+
+contador = 0
