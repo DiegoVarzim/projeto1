4062c2907e1bde570c89950a09944d9a54e4f32b
Diego Varzim <diego.varzim@hotmail.com>
2023-03-15 13:35:50 -0300

    atualização do dia 15/03/23

--- aula40.py
+++ aula40.py
@@ -1,4 +1,13 @@
 """
 Calculadora com while (enquanto/looping)
 
+.lower - converte tudo que for minúsculo para maiúsculo.
+
 """
+
+while True:
+    print('')
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
