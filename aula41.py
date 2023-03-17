1c12cb944389fc9668f1473f68604862b40040ac
Diego Varzim <diego.varzim@hotmail.com>
2023-03-17 10:56:23 -0300

    atualização do dia 17/03/23

--- aula41.py
+++ aula41.py
@@ -1,1 +1,20 @@
-print('Agora eu sou Desenvolvedor!!!')
+""" while/else  - enquato/outro
+"""
+
+string = 'Valorqualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+
+    if letra == ' ':
+        break                  # parar o código quando encontrar um espaço em branco
+
+    print(letra)
+    i += 1
+
+else:
+    print('Não encontrei um espaço na string')
+print('Fora do while.')
+
